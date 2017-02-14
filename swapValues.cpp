3166931415a778e02e864ecaf837cce2b43f4377
Vadim Brik <Brik99brik@gmail.com>
2017-02-14 14:55:00 +0600

    Create swapValues.cpp

---
+++ swapValues.cpp
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2017 Brik Vadim - All Rights Reserved
+ * https://github.com/BrikVadim/swapValues
+ */
+
+template <typename T> inline void swapValues(T & a, T & b);
+
+template <typename T>
+inline void swapValues(T & a, T & b) {
+	a += 2 * (b - a);
+	b -= (a - b);
+	a -= (a - b) / 2;
+}
